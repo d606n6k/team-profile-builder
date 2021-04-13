9b9da7fc670bb303f8cc7ea97706b39163fd24be
William Lucht <aaronlucht@gmail.com>
2021-04-12 17:11:08 -0700

    html prototype intial start

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,37 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    // initialization group
+    describe("initialization", () => {
+        it("should have a name", () => {
+            // arrange
+            const name = "William";
+            // act
+            const emp = new Employee(name);
+            // assert
+            expect(emp.name).toBe(name);
+            expect(emp.id).toBe(id);
+            expect(emp.email).toBe(email);
+        });
+    });
+    // name
+    // id
+    // email
+
+// describe getName()
+describe("getName()", () => {
+    it("should return the name", ()=> {
+        // arr
+        const name = "Some Name";
+        const emp = new Employee(name, 1, "fake@email.com");
+
+        // act
+        const results = emp.getName();
+        // assert
+        expect(results).toBe(name);
+    })
+})
+// getId()
+// getEmail()
+// getRole()—returns 'Employee'
+});
